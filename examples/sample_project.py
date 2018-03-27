572470074c4f0de288c6defa304d91cefd9bf64e
Szabolcs Dombi <cprogrammer1994@gmail.com>
2018-03-27 18:53:22 +0300

    duplicate source

--- examples/sample_project.py
+++ examples/sample_project.py
@@ -2,7 +2,7 @@ from cfly import build_module
 
 mymodule = build_module(
     'mymodule',
-    sources=['examples/sample_project/module.cpp', 'examples/sample_project/something.cpp'],
+    sources=['examples/sample_project/something.cpp'],
     preprocess=['examples/sample_project/module.cpp'],
 )
 
